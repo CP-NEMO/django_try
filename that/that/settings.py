79c22ea91c1ea8ee6729e9c973e5d74f797045b3
Aabhas Mathur <aabhasmathur5@gmail.com>
2021-04-16 12:37:47 +0530

    Update settings.py

--- that/that/settings.py
+++ that/that/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
